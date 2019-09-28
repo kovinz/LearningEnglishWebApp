43341df8c2b13cc46313297ac56a7a77090201e4
Roman Nevyantsev <kovin44@gmail.com>
2019-09-28 22:23:29 +0300

    added new method for finding user by nickname

--- backend/src/main/java/ru/kovin/web_app_for_learning_english/repositories/UserRepository.java
+++ backend/src/main/java/ru/kovin/web_app_for_learning_english/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kovin.web_app_for_learning_english.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+  User findByNickname(String userName);
 }
