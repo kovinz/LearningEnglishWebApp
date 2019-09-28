57f306836003bfc794078522d96cb39488bdfa76
Roman Nevyantsev <kovin44@gmail.com>
2019-09-28 22:26:04 +0300

    created a simple class which implements UserDetailsService and allows to load user by username (needed for security)

---
+++ backend/src/main/java/ru/kovin/web_app_for_learning_english/security/UserRepositoryUserDetailsService.java
@@ -0,0 +1,33 @@
+package ru.kovin.web_app_for_learning_english.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.
+        UserDetailsService;
+import org.springframework.security.core.userdetails.
+        UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.kovin.web_app_for_learning_english.entities.User;
+import ru.kovin.web_app_for_learning_english.repositories.UserRepository;
+
+@Service
+public class UserRepositoryUserDetailsService
+        implements UserDetailsService {
+  private UserRepository userRepo;
+
+  @Autowired
+  public UserRepositoryUserDetailsService(UserRepository userRepo) {
+    this.userRepo = userRepo;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username)
+          throws UsernameNotFoundException {
+    User user = userRepo.findByNickname(username);
+    if (user != null) {
+      return user;
+    }
+    throw new UsernameNotFoundException(
+            "User '" + username + "' not found");
+  }
+}
