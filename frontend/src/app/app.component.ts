12af71e6ef5e7fd119a905324b61af554201df49
Roman Nevyantsev <kovin44@gmail.com>
2019-09-29 17:04:18 +0300

    changed the top of site

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
- 
+import {Component} from '@angular/core';
+import {User} from './model/user';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
+
   title: string;
- 
+  currentUser: User;
+
   constructor() {
-    this.title = 'Spring Boot - Angular Application';
+    this.title = 'Web Portal';
   }
+
+  public setUser(user: User) {
+    this.currentUser = user;
+  }
+
+  public getUser() {
+    return this.currentUser;
+  }
+
 }
