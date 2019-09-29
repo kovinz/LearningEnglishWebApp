9d58daac6471255779cc027da9fef3eee3b79490
Roman Nevyantsev <kovin44@gmail.com>
2019-09-29 16:46:56 +0300

    created first page and first version of registration page

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UserFormComponent } from './user-form/user-form.component';
+ 
+const routes: Routes = [
+  { path: 'adduser', component: UserFormComponent }
+];
+ 
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
