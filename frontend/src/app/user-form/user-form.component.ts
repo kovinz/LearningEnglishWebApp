1f3de30de935519d13d2d487514782755a743428
Roman Nevyantsev <kovin44@gmail.com>
2019-09-29 17:05:42 +0300

    made the page of signin look pretier

--- frontend/src/app/user-form/user-form.component.ts
+++ frontend/src/app/user-form/user-form.component.ts
@@ -1,25 +1,34 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { UserService } from '../service/user.service';
-import { User } from '../model/user';
- 
+import {Component} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserService} from '../service/user.service';
+import {User} from '../model/user';
+import {AppComponent} from '../app.component';
+
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent {
- 
+
   user: User;
- 
-  constructor(private route: ActivatedRoute, private router: Router, private userService: UserService) {
+
+  constructor(private route: ActivatedRoute, private router: Router, private userService: UserService, private appComponent: AppComponent) {
     this.user = new User();
   }
- 
+
   onSubmit() {
-    this.userService.save(this.user).subscribe(result => this.gotoUserList());
+    const tmpUser = this.userService.checkUser(this.user);
+    tmpUser.subscribe(u => {
+      u.forEach(user => {
+        if (user.password === this.user.password) {
+          this.appComponent.setUser(user);
+          this.gotoUserList();
+        }
+      });
+    });
   }
- 
+
   gotoUserList() {
     this.router.navigate(['/users']);
   }
