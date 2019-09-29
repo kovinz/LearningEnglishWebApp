0c796c1e03dbf9f54435411cf33ec11007862e2d
Roman Nevyantsev <kovin44@gmail.com>
2019-09-29 17:05:00 +0300

    changed route to signin

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { UserFormComponent } from './user-form/user-form.component';
- 
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {UserFormComponent} from './user-form/user-form.component';
+
 const routes: Routes = [
-  { path: 'adduser', component: UserFormComponent }
+  {path: 'signin', component: UserFormComponent}
 ];
- 
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
