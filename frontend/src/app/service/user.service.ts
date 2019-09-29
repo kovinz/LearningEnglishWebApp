ca572873e3967fa9fba414cd3923dca42bff2eab
Roman Nevyantsev <kovin44@gmail.com>
2019-09-29 17:03:16 +0300

    added some todo methods

--- frontend/src/app/service/user.service.ts
+++ frontend/src/app/service/user.service.ts
@@ -5,18 +5,34 @@ import { Observable } from 'rxjs';
  
 @Injectable()
 export class UserService {
- 
+
   private usersUrl: string;
- 
+  private login: string;
+  private filter: string;
+
   constructor(private http: HttpClient) {
-    this.usersUrl = 'http://localhost:8080/users';
+    this.usersUrl = 'http://localhost:8080/users/';
+    this.login = 'login/';
+    this.filter = 'filter/';
   }
- 
+
   public findAll(): Observable<User[]> {
     return this.http.get<User[]>(this.usersUrl);
   }
- 
+
   public save(user: User) {
     return this.http.post<User>(this.usersUrl, user);
+  }
+
+  public update(user: User) {
+    return this.http.put<User>(this.usersUrl, user);
+  }
+
+  public checkUser(user: User) {
+    return this.http.get<User[]>(this.usersUrl + this.login + user.nickname);
+  }
+
+  public filterByLogin(user: User) {
+    return this.http.get<User[]>(this.usersUrl + this.filter + this.login + user.nickname);
   }
 }
